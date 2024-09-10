591ab9196d3de1c407153200b9e3e416a5f3a4f0
Santhosh13072006 <santhoshreddy1307@gmail.com>
2024-09-10 19:19:03 +0530

    Create Prime or not prime.c

---
+++ Prime or not prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a;
+    printf("if result is not specified then it is prime\n");
+    printf("enter a number:");
+    scanf("%d",&n);
+    {
+        for(i=2;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                printf("result=not prime");
+                break;
+            }
+        }
+    }
+  
+    return 0;
+}
