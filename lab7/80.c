2adb664278dca2a9afcaf8a022daa4ce12521ef5
Santhosh13072006 <santhoshreddy1307@gmail.com>
2024-11-04 16:29:27 +0530

    Create 80.c

---
+++ lab7/80.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<math.h>
+int function(int x)
+{
+    int a=0,b=1,c,i=2;
+    if(x==1)
+    {
+        printf("the 1 term of fibonacii seris is %d",a);
+    }
+    else if(x==1)
+    {
+        printf("the 1 term of fibonacii series is %d",a);
+         printf("the 2 term of fibonacii series is %d",b);
+    }
+    else 
+    {
+         printf("the 1 term of fibonacii series is %d\n",a);
+         printf("the 2 term of fibonacii series is %d\n",b);
+        do
+        {
+            c=a+b;
+            printf("the %d term of fabinocii series is %d\n ",i+1,c);
+            a=b;
+            b=c;
+            i=i+1;
+        }while(i<=x-1);
+    }
+}
+
+int main()
+{
+  int n;
+  printf("enter a number:");
+  scanf("%d",&n);
+  function(n);
+  return 0;
+ 
+     
+}
