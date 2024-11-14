2891ab4302287e6fda96d33604dfb61a23c14602
Santhosh13072006 <santhoshreddy1307@gmail.com>
2024-11-14 16:05:28 +0530

    Create 84.c

---
+++ lab7/84.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <string.h>
+void f(void);
+int main()
+{
+f();
+    
+
+    return 0;
+}
+void f(void)
+{
+    int s[100];
+    printf("enter a string\n");
+    gets(s);
+    printf("%s",s);
+}
