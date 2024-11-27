dc2c139a15070693c3e8b62a6bb83b4fa8011042
Santhosh13072006 <santhoshreddy1307@gmail.com>
2024-11-27 16:27:57 +0530

    Update 99.c

--- lab8/99.c
+++ lab8/99.c
@@ -1,31 +1,46 @@
-// Online C compiler to run C program online
-#include<stdio.h>
-#include<string.h>
-void function(void)
-{
-    
-    char s[100];
-    int c=0,x,i,l;
-    printf("enter a string:");
-    gets(s);
-    l=strlen(s);
-    for(i=0;i<l;i++)
-    {
-       x=s[i] ;
-    
-    if(x==65||x==97||x==69||x==101||x==73||x==105||x==79||x==111||x==85||x==117)
+
+#include <stdio.h>
+int f(char x)
+{ static int c=0;
+   if(x==65||x==97||x==69||x==101||x==73||x==105||x==79||x==111||x==85||x==117)
     {
     c=c+1;
     }
-    }
-
-    printf("%d",c);
+    return (c);
 }
 
-int main()
+
+int main() 
 {
-  function();
-  
-  
-  return 0;
+    FILE *fp;
+    int r;
+    char c;
+    
+    printf("enter a string\n");
+    fp=fopen("abc.txt","w");
+    if(fp==NULL)
+    {
+        printf("error opening file for writing");
+        return 1;
+    }
+    while((c=getchar())!='\0')
+    {
+        putc(c,fp);
+        
+    }
+    fclose(fp);
+    fp=fopen("abc.txt","r");
+     if(fp==NULL)
+    {
+        printf("error opening file for reading");
+        return 1;
+    }
+     while((c=getc(fp))!=EOF)
+    {
+        r=f(c);
+        
+    }
+    fclose(fp);
+    printf(" no.of vowels %d",r);
+    return 0;
 }
